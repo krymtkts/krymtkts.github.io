dda960bbb50b1ef30e32ce8d66c71e2182db0e24
krymtkts <takatoshi.kuriyama@gmail.com>
2026-09-13 10:13:12 +0900

    Expose Pagefind filters in the search modal.

--- src/Generator.fs
+++ src/Generator.fs
@@ -170,12 +170,38 @@ module Generation =
                         prop.custom ("bundle-path", $"%s{pathRoot}/pagefind/")
                         prop.custom ("base-url", $"%s{pathRoot}/")
                         prop.custom ("lang", "en")
+                        prop.custom ("faceted", "true")
                     ]
                     HtmlHelper.createElement "pagefind-modal-trigger" [
                         prop.custom ("shortcut", "/")
                         prop.custom ("compact", "true")
                     ]
-                    HtmlHelper.createElement "pagefind-modal" []
+                    HtmlHelper.createElement "pagefind-modal" [
+                        prop.children [
+                            HtmlHelper.createElement "pagefind-modal-header" [
+                                prop.children [ HtmlHelper.createElement "pagefind-input" [] ]
+                            ]
+                            HtmlHelper.createElement "pagefind-modal-body" [
+                                prop.children [
+                                    HtmlHelper.createElement "pagefind-filter-dropdown" [
+                                        prop.custom ("filter", "section")
+                                        prop.custom ("label", "Section")
+                                        prop.custom ("single-select", "true")
+                                    ]
+                                    HtmlHelper.createElement "pagefind-filter-dropdown" [
+                                        prop.custom ("filter", "tag")
+                                        prop.custom ("label", "Tag")
+                                        prop.custom ("single-select", "true")
+                                    ]
+                                    HtmlHelper.createElement "pagefind-summary" []
+                                    HtmlHelper.createElement "pagefind-results" []
+                                ]
+                            ]
+                            HtmlHelper.createElement "pagefind-modal-footer" [
+                                prop.children [ HtmlHelper.createElement "pagefind-keyboard-hints" [] ]
+                            ]
+                        ]
+                    ]
                 ]),
         navs |> Seq.choose toSitemap
 
